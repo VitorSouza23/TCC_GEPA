418497936cc3a0a0bd9028f423893decb029bb2b
Vitor Souza <vitordesolza@gmail.com>
2018-08-14 22:43:43 -0300

    Conclusão da implementação: #9 Criar o projeto de Ressources e #17 Criar projeto de Teste de unidade

---
+++ Gepa/Gepa.Resoucers/Language.cs
@@ -0,0 +1,34 @@
+﻿using Gepa.Resources.Languages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gepa.Resources
+{
+    public class Language
+    {
+
+        protected static string GetString(string stringName)
+        {
+            switch (Thread.CurrentThread.CurrentCulture.Name)
+            {
+                default:
+                    return pt_BR.ResourceManager.GetString(stringName);
+            }
+        }
+
+        /// <summary>
+        /// Gepa
+        /// </summary>
+        public static string Gepa
+        {
+            get
+            {
+                return GetString("Gepa");
+            }
+        }
+    }
+}
---
+++ Gepa/Gepa.UnitTest/Gepa.UnitTests/LanguageTests/PtBRTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gepa.Resources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gepa.UnitTests
+{
+    [TestClass]
+    public class PtBrTests
+    {
+        [TestMethod]
+        public void Gepa_Test()
+        {
+            var expectedVlaue = "Gepa";
+            var resultValue = Language.Gepa;
+            Assert.AreEqual(expectedVlaue, resultValue);
+        }
+    }
+}

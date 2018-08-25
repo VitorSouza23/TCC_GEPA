755e0c4fd8a99d09f236cdc45e7cd34587534916
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2018-08-25 00:39:15 -0300

    Adicionando e Configurando AutoMapper

---
+++ Gepa/Gepa.Site/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gepa.Site.Mappers
+{
+    public class AutoMapperConfig
+    {
+        public static void ConfigureMappings()
+        {
+            
+        }
+    }
+}

77fa84f24d5bf725237e2414bd96bbd0d96b896f
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2018-08-25 01:03:31 -0300

    Realizando a configuração e adicão da classe AutoMapperConfig
    Itens de Trabalho Relacionados: #7

--- Gepa/Gepa.Site/Global.asax.cs
+++ Gepa/Gepa.Site/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gepa.Site.Mappers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace Gepa.Site
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.ConfigureMappings();
         }
     }
 }
